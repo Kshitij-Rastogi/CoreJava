de51f9b18abeccd29a9305e480fba35da7c78421
Kshitij Rastogi <58160352+Kshitij-Rastogi@users.noreply.github.com>
2020-12-13 23:55:54 +0530

    Add files via upload

---
+++ challenge-1-boilerplate/TemperatureConverter.java
@@ -0,0 +1,11 @@
+public class TemperatureConverter{
+    public static void main(String[] args) {
+        int tempCelsius = 0;
+        System.out.println("Sr. No"+ "\t\t" + "Temperature(Celsius)" + "\t\t" + "Temperature(Fahrenheit)");
+        for(int i = 1 ; i<=11 ; i++){
+            int tempFahrenheit = (tempCelsius * 9/5) + 32;
+            System.out.println(i + "\t\t\t" + tempCelsius + "\t\t\t\t" + tempFahrenheit);
+            tempCelsius += 10;
+        }
+    }
+}
